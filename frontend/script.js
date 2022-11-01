880ba922471723d5d680bee060e4fd00df18ce5d
Harrison G <robotgeissler@gmail.com>
2022-10-31 22:32:37 -0400

    Added scrolling background and nav, ran into issues with a crossing fading background being cluncky to implement

--- frontend/script.js
+++ frontend/script.js
@@ -14,8 +14,22 @@ const listItems = [];
 
 let dragStartIndex;
 
-createList();
+var imageList = [
+    "../images/DALL·E 2022-10-21 09.43.54 - A fancy kitchen from a children's book.png",
+    "../images/DALL·E 2022-10-21 09.43.58 - A fancy kitchen from a children's book.png",
+    "../images/DALL·E 2022-10-21 09.44.00 - A fancy kitchen from a children's book.png",
+    "../images/DALL·E 2022-10-21 09.46.46 - A fancy expressionist oil painting of a fancy italian kitchen in a childrens book.png",
+    "../images/DALL·E 2022-10-21 09.46.49 - A fancy expressionist oil painting of a fancy italian kitchen in a childrens book.png",
+    "../images/DALL·E 2022-10-21 09.46.51 - A fancy expressionist oil painting of a fancy italian kitchen in a childrens book.png"
+  ];
+var imgIndex = 0;
 
+// Can't find a scalable method for cross-fading backgrounds
+
+function swapBackground(){
+    $('#backdrop-rotator').css('background-image', 'url("' + imageList[imgIndex] + '")');
+    imgIndex = (imgIndex + 1) % (imageList.length);
+}
 
 function createList() {
     [...recipe]
@@ -99,5 +113,12 @@ function addEventListeners() {
         item.addEventListener('dragleave', dragLeave);
     });
 }
+
+$(document).ready(function(){
+    swapBackground()
+    setInterval(swapBackground, 17000);
+});
+
+createList();
 
 check.addEventListener('click', checkOrder);
