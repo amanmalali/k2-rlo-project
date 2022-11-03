e94f579430e10aeb3e035740d094dcb39272b145
Sidharth <sidharthanup@gmail.com>
2022-11-03 02:34:57 -0400

    Add backend flask skeleton
    Signed-off-by: Sidharth <sidharthanup@gmail.com>

---
+++ api/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__, template_folder="../frontend", static_folder="../frontend/static")
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# @app.route('/static/script.js')
+# def script():
+#      return render_template('/static/script.js')
+
+@socketio.on('my event')
+def test_message(message):
+    emit('my response', {'data': message['data']})
+
+@socketio.on('my broadcast event')
+def test_message(message):
+    emit('my response', {'data': message['data']}, broadcast=True)
+
+@socketio.on('connect')
+def test_connect():
+    emit('my response', {'data': 'Connected'})
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
+
+if __name__ == '__main__':
+    socketio.run(app)
