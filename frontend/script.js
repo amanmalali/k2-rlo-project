b4a1d2be3ee71fcadc0d4e0365c4b0a68c39658b
Harrison G <robotgeissler@gmail.com>
2022-11-07 22:29:57 -0500

    Beautifying and adding links

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const draggableList = document.getElementById("draggable-list");
+const draggableList = document.getElementById("game-front");
 const check = document.getElementById("check");
 
 const recipe = [
@@ -13,23 +13,6 @@ const recipe = [
 const listItems = [];
 
 let dragStartIndex;
-
-var imageList = [
-    "../images/DALL·E 2022-10-21 09.43.54 - A fancy kitchen from a children's book.png",
-    "../images/DALL·E 2022-10-21 09.43.58 - A fancy kitchen from a children's book.png",
-    "../images/DALL·E 2022-10-21 09.44.00 - A fancy kitchen from a children's book.png",
-    "../images/DALL·E 2022-10-21 09.46.46 - A fancy expressionist oil painting of a fancy italian kitchen in a childrens book.png",
-    "../images/DALL·E 2022-10-21 09.46.49 - A fancy expressionist oil painting of a fancy italian kitchen in a childrens book.png",
-    "../images/DALL·E 2022-10-21 09.46.51 - A fancy expressionist oil painting of a fancy italian kitchen in a childrens book.png"
-  ];
-var imgIndex = 0;
-
-// Can't find a scalable method for cross-fading backgrounds
-
-function swapBackground(){
-    $('#backdrop-rotator').css('background-image', 'url("' + imageList[imgIndex] + '")');
-    imgIndex = (imgIndex + 1) % (imageList.length);
-}
 
 function createList() {
     [...recipe]
@@ -113,11 +96,6 @@ function addEventListeners() {
         item.addEventListener('dragleave', dragLeave);
     });
 }
-
-$(document).ready(function(){
-    swapBackground()
-    setInterval(swapBackground, 17000);
-});
 
 createList();
 
