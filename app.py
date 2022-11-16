1a77c5bea0b50d716abc5c8c6fde58637f51723e
Sidharth <sidharthanup@gmail.com>
2022-11-16 15:05:58 -0500

    Add backend handling for role creation, create recipe and on successfull ordering

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 from flask_socketio import SocketIO, emit
+from flask_session import Session
+from uuid import uuid4
 
 app = Flask(__name__, template_folder="./frontend/template", static_folder="./frontend/static")
 app.config['SECRET_KEY'] = 'secret!'
@@ -9,25 +11,43 @@ socketio = SocketIO(app)
 def index():
     return render_template('index.html')
 
+app.config['SECRET_KEY'] = uuid4().hex
+
+app.config["SESSION_PERMANENT"] = True
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+socketio = SocketIO(app)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 # @app.route('/static/script.js')
 # def script():
 #      return render_template('/static/script.js')
 
-@socketio.on('my event')
-def test_message(message):
-    emit('my response', {'data': message['data']})
+@socketio.on('play_as_teacher')
+def play_as_teacher():
+    session["role"] = "teacher"
+    emit('teacher_joined_response', {'data': "teacher has joined"} broadcast=True)
 
-@socketio.on('my broadcast event')
-def test_message(message):
-    emit('my response', {'data': message['data']}, broadcast=True)
+@socketio.on('play_as_student')
+def play_as_student():
+    session["role"] = "student"
+    emit('student_joined_response', {'data': "student has joined"}, broadcast=True)
 
-@socketio.on('connect')
-def test_connect():
-    emit('my response', {'data': 'Connected'})
+@socketio.on('create_recipe')
+def create_recipe(recipe):
+    if session["role"] == "teacher":
+        session["recipe"] = recipe
+        #Clients get the unshuffled (actual) recipe. It is upto the client to shuffle it.
+        emit('new recipe added', {'data': recipe}, broadcast=True)
 
-@socketio.on('disconnect')
-def test_disconnect():
-    print('Client disconnected')
+@socketio.on('on_correct_ordering')
+def on_correct_ordering():
+    if session["role"] == "student":
+        # When teacher gets this message the client has to prompt the teacher to create a new recipe
+        emit('student has correctly ordered the recipe!', {'data' : True}, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app)
